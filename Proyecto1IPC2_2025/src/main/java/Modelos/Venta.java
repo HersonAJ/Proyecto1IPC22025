53b181726b06ea27ec716cbe7eab783c7af8033b
HersonAJ <hersonaguilar202230685@cunoc.edu.gt>
2025-02-23 17:51:40 -0600

    modelo para ventas

---
+++ Proyecto1IPC2_2025/src/main/java/Modelos/Venta.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author herson
+ */
+public class Venta {
+    
+    private int idVenta;
+    private int idCliente;
+    private int idUsuario;
+    private Date fechaVenta;
+    private double totalVenta;
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public Date getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public double getTotalVenta() {
+        return totalVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setFechaVenta(Date fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public void setTotalVenta(double totalVenta) {
+        this.totalVenta = totalVenta;
+    }
+    
+}
