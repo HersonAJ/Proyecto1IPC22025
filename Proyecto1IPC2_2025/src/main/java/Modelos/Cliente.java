38b21be2a0c1f4663d7dd57d19b654f0bd670739
HersonAJ <hersonaguilar202230685@cunoc.edu.gt>
2025-02-22 23:07:08 -0600

    modelo para cliente

---
+++ Proyecto1IPC2_2025/src/main/java/Modelos/Cliente.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author herson
+ */
+public class Cliente {
+    
+    private int idCliente;
+    private String nit;
+    private String nombre;
+    private String direccion;
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+}
